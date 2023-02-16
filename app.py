86f7f2381be3f00fea5d7666c8b4f1989289a786
王建昊 <575822867@qq.com>
2023-02-16 21:38:34 +0800

    学习了部分前端知识

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/hello')
+def hello():
+    name = '张三'
+    return '%s,你好' % name
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
